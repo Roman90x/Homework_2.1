a68b85135e53d67c6adf7414cf78fca475c0c7a9
ROMAN <roman90x@icloud.com>
2022-11-06 02:08:07 +0600

    ДЗ1

---
+++ src/car.java
@@ -0,0 +1,9 @@
+public class car {
+
+    public String brand;
+    public String model;
+    public double engineVolume;
+    public String color;
+    public int productionYear;
+    public String productionCountry;
+}
